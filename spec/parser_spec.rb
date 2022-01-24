85d816b4a44bf653e808b5a17102a2d9883a7d59
irizaga <gibran.irizaga@gmail.com>
2022-01-24 08:49:59 -0300

    changes made to spec descriptions

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,20 +1,20 @@
 require_relative '../lib/parser'
 
 describe Parser do
-  describe 'file does not exist' do
-    it 'and matches error' do
+  describe '.exist?' do
+    it 'raises error' do
       expect { Parser.new('any.log') }.to raise_error('Arquivo não encontrado')
     end
   end
 
-  describe 'first line' do
+  describe '#read_first_line' do
     it 'matches string in file' do
       data = Parser.new('games.log')
       expect(data.read_first_line).to match("  0:00 ------------------------------------------------------------\n")
     end
   end
 
-  describe 'line count' do
+  describe '#line_count' do
     it 'returns correct output' do
       data = Parser.new('games.log')
       expect(data.line_count).to match("{\"games.log\":{\"lines\":5306}}")
